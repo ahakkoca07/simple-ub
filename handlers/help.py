d76e77d4643b8ed4c2cb86935011b9b400427abe
ahathedev <ahathedev@ahathe.dev>
2025-11-23 00:16:16 +0300

    help.py and start.py ready

---
+++ handlers/help.py
@@ -0,0 +1,25 @@
+from telethon import events
+
+def register(bot):
+    @bot.on(events.NewMessage(pattern='/help'))
+    async def help_handler(event):
+        # if in pms
+        if event.is_private:
+            await event.reply(
+f"""
+This is a placeholder text for /help command in pm's.
+""",
+parse_mode="markdown")
+        # if in a group
+        elif event.is_group:
+            await event.reply(
+f"""
+This is a placeholder text for /help command in groups.
+""",
+parse_mode="markdown")
+        elif event.is_channel:
+            await event.reply(
+f"""
+This is a placeholder text for /help command in channels.
+""",
+parse_mode="markdown")
